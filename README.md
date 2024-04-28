ec625e03bf8377d4902b35e096ae5c7e664f0dd5
BhanuprakashBandreddi <bband@my.bridgeport.edu>
2024-04-28 19:49:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# MealMatch
+
+A food delivery application made using React and Swiggy's API.
+
+- React
+- Parcel
+- Tailwind CSS
+- Jest(Testing)
+- Redux-toolkit
+- Get restaurants by current location
+- Add to cart
+- Shimmer UI
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div className='container-max text-center py-4 border-t'>
+      Foody, made by{' '}
+      <a
+        href='https://dineshrout.netlify.app'
+        target={'_blank'}
+        className='text-orange-500'
+      >
+        Bhanu Prakash Bandreddi
+      </a>
+    </div>
+  );
+};
+export default Footer;
